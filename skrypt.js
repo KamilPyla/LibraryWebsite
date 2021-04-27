8f988e6a06d91b1656079a9544d48a9eb4e0e7f1
KamilPyla <kamilpyla@gmail.com>
2021-04-27 10:40:51 +0200

    Update

---
+++ skrypt.js
@@ -0,0 +1,114 @@
+// funkcja szukająca 
+
+function search() {
+    var str = document.getElementById("phrase").value;
+    str = "<h1>" + str + "<h1/>";
+    document.getElementById("List").innerHTML = str;
+}
+
+function woje() {
+
+    var w = document.getElementById("woj").value;
+
+    if (w == "1") {
+
+        const citysDS = ["Bardo", "Bielawa", "Bierutów", "Bogatynia", "Boguszów-Gorce", "Bolesławiec", "Bolków", "Brzeg Dolny", "Bystrzyca Kłodzka", "Chocianów", "Chojnów", "Duszniki-Zdrój", "Dzierżoniów", "Głogów", "Głuszyca", "Góra (województwo dolnośląskie)", "Gryfów Śląski", "Jawor (miasto)", "Jaworzyna Śląska", "Jedlina-Zdrój", "Jelcz-Laskowice", "Jelenia Góra", "Kamieniec Ząbkowicki", "Kamienna Góra", "Karpacz", "Kąty Wrocławskie", "Kłodzko", "Kowary", "Kudowa-Zdrój", "Lądek-Zdrój", "Legnica", "Leśna", "Lubań (województwo dolnośląskie)", "Lubawka", "Lubin", "Lubomierz", "Lwówek Śląski", "Mieroszów", "Międzybórz", "Międzylesie", "Milicz", "Mirsk", "Niemcza", "Nowa Ruda", "Nowogrodziec", "Oborniki Śląskie", "Oleśnica", "Olszyna", "Oława", "Piechowice", "Pieńsk", "Pieszyce", "Piława Górna", "Polanica-Zdrój", "Polkowice", "Prochowice", "Prusice", "Przemków", "Radków", "Siechnice", "Sobótka (powiat wrocławski)", "Stronie Śląskie", "Strzegom", "Strzelin", "Syców", "Szczawno-Zdrój", "Szczytna", "Szklarska Poręba", "Ścinawa", "Środa Śląska", "Świdnica", "Świebodzice", "Świeradów-Zdrój", "Świerzawa", "Trzebnica", "Twardogóra", "Wałbrzych", "Wąsosz (województwo dolnośląskie)", "Węgliniec", "Wiązów", "Wleń", "Wojcieszów", "Wołów", "Wrocław", "Zawidów", "Ząbkowice Śląskie", "Zgorzelec", "Ziębice", "Złotoryja", "Złoty Stok", "Żarów", "Żmigród"];
+        var s = "";
+        console.log("Krowa")
+        for (i = 0; i < citysDS.length(); i++) {
+            s = s + '<option>' + citysDS[i] + '</option>';
+        }
+        document.getElementById("city").innerHTML = s;
+
+
+    } else if (w == 2) {
+
+
+        const citysDS = ["Bardo", "Bielawa", "Bierutów", "Bogatynia", "Boguszów-Gorce", "Bolesławiec", "Bolków", "Brzeg Dolny", "Bystrzyca Kłodzka", "Chocianów", "Chojnów", "Duszniki-Zdrój", "Dzierżoniów", "Głogów", "Głuszyca", "Góra (województwo dolnośląskie)", "Gryfów Śląski", "Jawor (miasto)", "Jaworzyna Śląska", "Jedlina-Zdrój", "Jelcz-Laskowice", "Jelenia Góra", "Kamieniec Ząbkowicki", "Kamienna Góra", "Karpacz", "Kąty Wrocławskie", "Kłodzko", "Kowary", "Kudowa-Zdrój", "Lądek-Zdrój", "Legnica", "Leśna", "Lubań (województwo dolnośląskie)", "Lubawka", "Lubin", "Lubomierz", "Lwówek Śląski", "Mieroszów", "Międzybórz", "Międzylesie", "Milicz", "Mirsk", "Niemcza", "Nowa Ruda", "Nowogrodziec", "Oborniki Śląskie", "Oleśnica", "Olszyna", "Oława", "Piechowice", "Pieńsk", "Pieszyce", "Piława Górna", "Polanica-Zdrój", "Polkowice", "Prochowice", "Prusice", "Przemków", "Radków", "Siechnice", "Sobótka (powiat wrocławski)", "Stronie Śląskie", "Strzegom", "Strzelin", "Syców", "Szczawno-Zdrój", "Szczytna", "Szklarska Poręba", "Ścinawa", "Środa Śląska", "Świdnica", "Świebodzice", "Świeradów-Zdrój", "Świerzawa", "Trzebnica", "Twardogóra", "Wałbrzych", "Wąsosz (województwo dolnośląskie)", "Węgliniec", "Wiązów", "Wleń", "Wojcieszów", "Wołów", "Wrocław", "Zawidów", "Ząbkowice Śląskie", "Zgorzelec", "Ziębice", "Złotoryja", "Złoty Stok", "Żarów", "Żmigród"];
+        var s = "";
+        for (i = 0; i < citysDS.length(); i++) {
+            s = s + '<option>' + citysDS[i] + '</option>';
+        }
+        document.getElementById("city").innerHTML = s;
+        // document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+    } else if (w == 3)
+
+
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 4)
+
+
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 5)
+
+
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 6)
+
+
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 7)
+
+
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 8)
+
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 9)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 10)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 11)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 12)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 13)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 14)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 15)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+    else if (w == 16)
+        document.getElementById("city").innerHTML = "<option>" + w + "</option>";
+
+
+
+    document.getElementById("check").innerHTML = w
+
+}
+
+function pass() {
+    var pass1 = document.getElementById("pass").value;
+    var pass2 = document.getElementById("passRep").value;
+    if (pass1 == pass2)
+        document.getElementById("correctPass").innerHTML = "hasła są takie same";
+    else
+        document.getElementById("correctPass").innerHTML = "hasła nie są identyczne.";
+}
